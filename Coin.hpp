1944ec6bb9f1b0ba7c435defb81acd57ffa8183b
ShreeyaVenneti <77499226+ShreeyaVenneti@users.noreply.github.com>
2021-12-28 11:20:29 +0530

    Update Coin.hpp

--- Coin.hpp
+++ Coin.hpp
@@ -3,11 +3,9 @@
 class Coin {
 public:
 	Coin();
-	virtual void generateObstacle(Window* window);
 	virtual void draw(SDL_Renderer* renderer, Window* window);
 	virtual void handleCollision();
 	virtual ~Coin();
-	virtual int ObstacleID();
 private:
 	int stage;
 	int ticks;
